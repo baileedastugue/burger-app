35ed89ada65e43acf53c30ef505a024df2a0ed45
Bailee Dastugue <bailee.dast@gmail.com>
2020-05-01 09:40:17 -0500

    added html skeleton to main.handlebars

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 var exphbs = require("express-handlebars");
 
-// app.engine("handlebars", exphbs({defaultLayout: "main"}));
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
