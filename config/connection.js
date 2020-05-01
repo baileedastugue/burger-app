45cfe3fbe20fee2d67ea975e3541442d63a11673
Bailee Dastugue <bailee.dast@gmail.com>
2020-05-01 16:52:21 -0500

    changed connection.js to work with jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 // code to connect Node to MySQL
 require("dotenv").config();
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: process.env.PASSWORD,
     database: "burgers_db"
-});
+    });
+};
+    
 
 // make connection
 connection.connect(function(err){
