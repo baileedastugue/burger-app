de66bc2b8cae678c771be1a26b3c11f56b55fb90
Bailee Dastugue <bailee.dast@gmail.com>
2020-05-01 09:53:28 -0500

    burger name and burger ids render to local host homepage

--- models/burger.js
+++ models/burger.js
@@ -8,7 +8,7 @@ var burger = {
             cb(res);
         });
     },
-    insertOne: function(cols, vals, cd) {
+    insertOne: function(cols, vals, cb) {
         orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
